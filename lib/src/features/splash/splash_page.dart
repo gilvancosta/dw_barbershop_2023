3fffb06223ebef7b8abcdc5726362f1f9b0243d7
Gilvan Costa <gilvancosta2025@gmail.com>
2023-08-16 17:03:48 -0300

    adicionando animações na splash_page

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,8 +1,32 @@
 // import 'package:asyncstate/asyncstate.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  var _scale = 10.0;
+  var _animationDuration = const Duration(milliseconds: 3000);
+  var _animationOpacityLogo = 0.0;
+
+  double get _logoAnimationWidth => _scale * 100;
+  double get _logoAnimationHeight => _scale * 120;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacityLogo = 1.0;
+        _scale = 1.0;
+      });
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +41,20 @@ class SplashPage extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Image.asset('assets/images/imgLogo.png'),
+          child: AnimatedOpacity(
+              opacity: _animationOpacityLogo,
+              curve: Curves.easeIn,
+              duration: _animationDuration,
+              child: AnimatedContainer(
+                duration: _animationDuration,
+                width: _logoAnimationWidth,
+                height: _logoAnimationHeight,
+                curve: Curves.linearToEaseOut,
+                child: Image.asset(
+                  'assets/images/imgLogo.png',
+                  fit: BoxFit.cover,
+                ),
+              )),
         ), // tive que adicionar para a imagem aparecer
       ),
     );
